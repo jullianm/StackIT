a8b44928de68c1dfb416d91872e04de60792635e
Jullian Mercier <jllnmercier@gmail.com>
2020-07-25 09:06:59 -0400

    Initial Commit

---
+++ StackIT/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  StackIT
+//
+//  Created by Jullian Mercier on 2020-07-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StackIT/StackITApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackITApp.swift
+//  StackIT
+//
+//  Created by Jullian Mercier on 2020-07-25.
+//
+
+import SwiftUI
+
+@main
+struct StackITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
